7fddf9e9b83509a18b5d5724e3e5693685d079e0
Javier Rodriguez <Javier.Rodriguez@azure.techlensglobal.com>
2023-12-18 09:56:31 +0100

    build: :construction_worker: Update builder action
    Builder action now uses env variable for header

--- builderPlugin.js
+++ builderPlugin.js
@@ -3,34 +3,39 @@ import { readFileSync, writeFileSync } from 'fs';
 /**
  * ConcatAndReplacePlugin concatenates files and replaces {{version}} with version from package.json
  * @param {Object} options plugin options
- * @param {string[]} options.files source files to concat
+ * @param {string} options.file source file
  * @param {string} options.packageJsonPath path to package.json to get version from
- * @param {string} options.destinationFile path to output file
  * @returns {void}
  */
 class ConcatAndReplacePlugin {
   constructor(options) {
-    const { files, packageJsonPath, destinationFile } = options;
-    this.files = files;
+    const { file, packageJsonPath } = options;
+    this.file = file;
     this.packageJsonPath = packageJsonPath;
-    this.destinationFile = destinationFile;
   }
 
   apply(compiler) {
     compiler.hooks.afterEmit.tapAsync('ConcatAndReplacePlugin', (_, callback) => {
+      const header = process.env.TAMPERMONKEY_HEADER;
 
-      // Concatenate files
-      const concatenatedContent = this.files
-        .map((file) => readFileSync(file, 'utf8'))
-        .join('\n');
+      if (!header) {
+        console.warn('[ConcatAndReplacePlugin]: tampermonkeyHeader env var not set, skipping header addition');
+        return callback();
+      }
 
-      // Replace {{version}} with version from package.json
+      // Replace {{package.*}} with values from package.json
       const packageJson = JSON.parse(readFileSync(this.packageJsonPath, 'utf8'));
-      let replacedContent = concatenatedContent.replace('{{package.version}}', packageJson.version);
-      replacedContent = replacedContent.replace('{{package.author}}', packageJson.author);
+      const regex = /{{package\.(.*?)}}/g;
+      let replacedHeader = header.replace(regex, (_, key) => packageJson[key]);
 
-      // Write to destination file
-      writeFileSync(this.destinationFile, replacedContent, { encoding: 'utf8', flag: 'w', });
+      if (!replacedHeader.endsWith('\n')) {
+        replacedHeader += '\n';
+      }
+
+      console.log(`[ConcatAndReplacePlugin]: writing header to ${this.file}`);
+      const fileContent = readFileSync(this.file, 'utf8');
+      const newFileContent = replacedHeader + fileContent;
+      writeFileSync(this.file, newFileContent, { encoding: 'utf8' });
 
       callback();
     });
